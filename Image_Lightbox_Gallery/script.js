ffe1fcee7df2e7f7cb1248696207811de8b73131
sujandev1635 <sujandev1635@gmail.com>
2023-09-17 02:46:46 +0600

    Image LightBox Gallery completed

---
+++ Image_Lightbox_Gallery/script.js
@@ -0,0 +1,20 @@
+let overlay = document.querySelector('.galleryOverlay');
+let imgBox = document.querySelector('.imgBox');
+var img = document.querySelector('.imgBox img');
+let close = document.querySelector('.imgBox span');
+
+let gallery = document.querySelector('#gallery');
+gallery.addEventListener('click', (e) => {
+  let currentImagePath = e.target.src;
+  if (currentImagePath !== undefined) {
+    overlay.classList.add('galleryOverlayShow');
+    imgBox.classList.add('imgBoxShow');
+    img.src = currentImagePath;
+    console.log(currentImagePath);
+  }
+});
+
+close.addEventListener('click', () => {
+  overlay.classList.remove('galleryOverlayShow');
+  imgBox.classList.remove('imgBoxShow');
+});
